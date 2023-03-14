c9e16d7249c0aaf8597e240d3f3df25bffd427f6
Aynkaran <aynkaran@makoitlab.com>
2023-03-14 17:40:56 +0530

    Changed image

--- assets/images/puzzle/index.ts
+++ assets/images/puzzle/index.ts
@@ -1,11 +1,11 @@
-import tile1 from "./1.jpg";
-import tile2 from "./2.jpg";
-import tile3 from "./3.jpg";
-import tile4 from "./4.jpg";
-import tile5 from "./5.jpg";
-import tile6 from "./6.jpg";
-import tile7 from "./7.jpg";
-import tile8 from "./8.jpg";
-import tile9 from "./9.jpg";
+import tile1 from "./1.png";
+import tile2 from "./2.png";
+import tile3 from "./3.png";
+import tile4 from "./4.png";
+import tile5 from "./5.png";
+import tile6 from "./6.png";
+import tile7 from "./7.png";
+import tile8 from "./8.png";
+import tile9 from "./9.png";
 
 export { tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9 };
